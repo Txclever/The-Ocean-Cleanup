2969cfeffb39ddcb5e963aeaadde8e8f86799d22
Txclever <yxt15850005248@outlook.com>
2021-05-25 17:55:57 +0800

    头部做了一半

---
+++ js/updates.js
@@ -0,0 +1,14 @@
+window.onscroll = function () {
+    let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    let Header = document.getElementById("header");
+    let ina = document.getElementById("inside-animation");
+    if (scrollTop > 50) {
+        Header.style.top = -75 + 'px';
+        ina.style.top = -75 + 'px';
+    } else {
+        Header.style.top = 0;
+        Header.style.display = "block";
+        ina.style.top = 0;
+        ina.style.display = "block";
+    }
+};

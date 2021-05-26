b8ce62f55f5e8f3e491114b75f089bb7082dfbbd
Txclever <yxt15850005248@outlook.com>
2021-05-26 22:20:37 +0800

    updates身体里的头部做完了

--- js/updates.js
+++ js/updates.js
@@ -2,6 +2,7 @@ window.onscroll = function () {
     let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
     let Header = document.getElementById("header");
     let ina = document.getElementById("inside-animation");
+
     if (scrollTop > 50) {
         Header.style.top = -75 + 'px';
         ina.style.top = -75 + 'px';
@@ -11,4 +12,11 @@ window.onscroll = function () {
         ina.style.top = 0;
         ina.style.display = "block";
     };
+}
+
+let remove_banner = document.getElementById("banner");
+let X = document.getElementById("X");
+
+X.onclick = function () {
+    remove_banner.remove(banner);
 }
